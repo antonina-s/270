a3d355d605eac744334e855db4d5df645ea345f7
Antonina S <138234225+antonina-s@users.noreply.github.com>
2023-08-05 18:19:24 +0300

    Delete LABS/4 Laba JS directory

--- LABS/4 Laba JS/7. Методы для перебора массивов/Методы для перебора массивов/09_10_Reduce_Intro/app-r_sas.js
+++
@@ -1,9 +0,0 @@
-// # Reduce Intro
-
-const nums = [3, 4, 5, 6, 7]; // 25
-
-// 1. Получите сумму элементо массива.
-const sum = nums.reduce((s, n) => {
-    return s + n;
-})
-console.log(`Сумма = ${sum}`);
